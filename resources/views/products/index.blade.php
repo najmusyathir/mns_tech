b5be5c2452652e0c4dd86468797880a27b9cf387
najmusyathir <alsyathir@gmail.com>
2024-04-28 08:02:03 +0800

    Update: System now can differ admin and user

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,7 +18,7 @@
         <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="{{asset('assets/images/setup.jpg')}}" />
         <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-            <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:justify-center lg:col-start-2">
                         <a href="{{route('home')}}">
                             <svg class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]" viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -49,22 +49,20 @@
 
                 <div class="content flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 ">
 
+                    @if (Auth::user()->user_type === 'admin')
                     <div class="icons" style="position: fixed; right:50px;bottom:50px; z-index:1">
                         <a href="{{route('products.add')}}">
                             <img src="{{asset('assets/icons/ic_plus.svg')}}" style="width: 40px;">
                         </a>
                     </div>
 
+                    @endif
                     <h1 class="font-semibold text-black dark:text-white" style="font-size: 2em;">Product List</h1>
 
 
                     <div class="grid gap-6 lg:grid-cols-3 lg:gap-8">
                         @foreach ($products as $product)
-                        <a id="docs-card" 
-                        
-                        href="{{route('products.details', ['id' => $product->id])}}" 
-                    
-                        class=" product flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">                            
+                        <a id="docs-card" href="{{route('products.details', ['id' => $product->id])}}" class=" product flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                             <div style="display: flex; flex-direction:row
                                 ">
                                 <div style="margin:10px">
@@ -73,7 +71,7 @@
                                     Stock: {{ $product->prod_stock }}
                                 </div>
                                 <div>
-                                <img src="{{ asset($product->prod_pic) }}" alt="{{ $product->prod_title }}" style="max-width: 100px; max-height: 100px;">
+                                    <img src="{{ asset($product->prod_pic) }}" alt="{{ $product->prod_title }}" style="max-width: 100px; max-height: 100px;">
 
                                 </div>
                             </div>
